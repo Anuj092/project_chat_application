fd3ad75f766192f08a95af3f5fe30c097c930980
Anuj Mishra <122159773+Anuj092@users.noreply.github.com>
2025-06-02 18:43:54 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -22,8 +22,8 @@ io.on('connect', (socket) => {
 
     socket.join(user.room);
 
-    socket.emit('message', { user: 'user', text: `${user.name}, welcome to room ${user.room}.`});
-    socket.broadcast.to(admin.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
 
     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
 
